f7b30af085c3e797fd933fda73f0b5711ca6d46b
Bhavana Chennu <bhavanachennu61@gmail.com>
2026-09-05 11:05:37 +0530

    Time: 2 ms (99.31%) | Memory: 44.3 MB (63.28%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0 , right = s.length()-1;
+        while( left < right){
+            if(!Character.isLetterOrDigit(s.charAt(left)))  left++;
+            else if(!Character.isLetterOrDigit(s.charAt(right))) right--;
+            else{
+                char leftChar = Character.toLowerCase(s.charAt(left));
+                char rightChar = Character.toLowerCase(s.charAt(right));
+                if( leftChar != rightChar){
+                        return false;
+                }
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}
